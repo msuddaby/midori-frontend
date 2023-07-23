957209f7edfea7f3f62697ba76dcd7b0f28222a7
Matthew Suddaby <msuddaby2@nait.ca>
2023-07-23 15:54:02 -0600

    relative?

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <>
-      <section className="green-bg min-h-screen min-w-screen text-white flex justify-center items-center p-12">
+      <section className="green-bg min-h-screen min-w-screen text-white flex justify-center items-center p-12 relative">
         <div className="flex flex-col items-center justify-center h-full text-center">
           <h1 className="text-6xl font-bold text-white">Midori Creative</h1>
         </div>
