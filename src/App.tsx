c3e6b49b1ef0f0ff4014610ee2b3009f27e352f9
Matthew Suddaby <msuddaby2@nait.ca>
2023-07-24 21:18:16 -0600

    gsap breaks on vercel.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import { useLayoutEffect } from "react";
 import "./App.css";
 import BestOf from "./sections/BestOf";
 import Friends from "./sections/Friends";
 import Hero from "./sections/Hero";
 import Offerings from "./sections/Offerings";
 import WhoIs from "./sections/WhoIs";
-import gsap from "gsap-trial";
-import { ScrollTrigger } from "gsap-trial/all";
-import { ScrollSmoother } from "gsap-trial/dist/ScrollSmoother";
+// import gsap from "gsap-trial";
+// import { ScrollTrigger } from "gsap-trial/all";
+// import { ScrollSmoother } from "gsap-trial/dist/ScrollSmoother";
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      ScrollSmoother.create({
-        smooth: 2,
-        effects: true,
-      });
-    });
+  // useLayoutEffect(() => {
+  //   const ctx = gsap.context(() => {
+  //     ScrollSmoother.create({
+  //       smooth: 2,
+  //       effects: true,
+  //     });
+  //   });
 
-    return () => ctx.revert();
-  }, []);
+  //   return () => ctx.revert();
+  // }, []);
 
   return (
     <>
